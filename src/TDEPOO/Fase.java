a868e46be405d33b0e7e9a0ff3fe1b681f40c3a1
Gabriel Fardoski <gabriel.fardoski@gmail.com>
2018-11-11 22:40:04 -0300

    Fase e Monstro modificados

--- src/TDEPOO/Fase.java
+++ src/TDEPOO/Fase.java
@@ -1,5 +1,7 @@
 package TDEPOO;
 
+import java.util.ArrayList;
+
 public class Fase {
     /*
     Essa classe é responsavel pela fase, ela ira guardar as informações de monstros, bosses e o nivel da mesma.
@@ -7,7 +9,23 @@ public class Fase {
 
     // Atributos
     private String local;
-    private double chanceEncontro;
+    private ArrayList<Monstro> monstros;  // Array de monstros comuns
+    private Monstro boss;                 // Boss da fase
+    private String raca;                  // Raca dos monstros da fase
+
+    // Atributos boolean armas
+    private boolean espada;
+    private boolean porrete;
+    private boolean machado;
+    private boolean lanca;
+    private boolean adaga;
+
+    // Arrays de frases de acordo com arma
+    private ArrayList<String> FrasesEspada;
+    private ArrayList<String> FrasesPorrete;
+    private ArrayList<String> FrasesMachado;
+    private ArrayList<String> FrasesLanca;
+    private ArrayList<String> FrasesAdaga;
 
     // Construtor
 
--- src/TDEPOO/Monstro.java
+++ src/TDEPOO/Monstro.java
@@ -9,11 +9,33 @@ public class Monstro {
     */
 
     // Atributos
-    private boolean boss;
-    private int vida;
-    private String raca;
+    private boolean boss; // Definirá se o boss é um boss ou não
+    private String raca;  // Raça
+    private Item drop;    // Item dropável do monstro             ---->             [if(!= boss){ lore == null}]
+    private String lore;  // Descrição/Lore somente existente se o monstro for Boss [if(!= boss){ lore == null}]
+
+    // Atributos de combate
+    private int vida;     // Vida do monstro
+    private int ataque;   // Dano Fisico do monstro
+    private int defesa;   // Defesa do monstro
 
     // Construtor
+
+    public Monstro(boolean boss, String raca, Item drop, String lore, int vida, int ataque, int defesa) {
+        this.boss = boss;
+        this.raca = raca;
+        this.drop = drop;
+        this.lore = lore;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+
+        if (boss == false) {
+
+        }
+
+
+    }
 
     // Métodos
 
